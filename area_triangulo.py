01db670142e1524f5e40d72e89b8a7eb4fcc7a18
Natalia <nibasor2309@g.educaand.es>
2025-10-09 12:19:55 +0200

    subo un programa que calcula el área de un triángulo

---
+++ area_triangulo.py
@@ -0,0 +1,6 @@
+base = 7.2 
+altura = 5.23
+
+area = base*altura/2 
+
+print ("El área del triángulo de lado 7.2 y altura 5.23 es", area)
---
+++ perimetro_cuadrado.py
@@ -0,0 +1,5 @@
+altura = 3 
+base = 7 
+
+perimetro = 2*base + 2*altura 
+print("el perímetro del cuadrado es", perimetro)
